cb0cf5bdc14d7c7814aa913dddf2fb5db3d8328b
Vlad <pleo12@gmail.com>
2015-12-08 22:28:24 +0200

    changed auth0 widget to auth0 lock library v7.12

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
-    var widget = new Auth0Widget({
-        domain: 'samples.auth0.com',
-        clientID: 'BUIJSW9x60sIHBw8Kd9EmCbj8eDIFxDC',
-        callbackURL: location.href,
-        callbackOnLocationHash: true
-    });
-    
+  var lock = new Auth0Lock(
+    'BUIJSW9x60sIHBw8Kd9EmCbj8eDIFxDC',
+    'samples.auth0.com');
+
     var userProfile;
 
     $('.btn-login').click(function(e) {
       e.preventDefault();
-      widget.signin({ popup: true} , null, function(err, profile, token) {
+      lock.show(function(err, profile, token) {
         if (err) {
           // Error callback
           console.log("There was an error");
@@ -18,9 +15,11 @@ $(document).ready(function() {
         } else {
           // Success calback
 
-          widget.getClient().getDelegationToken('IckaP4QRfGSRGuVZfP9VJBUdlXtgcS4o', token, 
+          lock.getClient().getDelegationToken({id_token: token},
             function(err, thirdPartyApiToken) {
-              localStorage.setItem('thirdPartyApiToken', thirdPartyApiToken.id_token);
+              if(err)
+              console.log(err);
+              else localStorage.setItem('thirdPartyApiToken', thirdPartyApiToken.id_token);
             });
 
           // Save the JWT token.
@@ -36,11 +35,11 @@ $(document).ready(function() {
       });
     });
 
-    
+
     $.ajaxSetup({
       'beforeSend': function(xhr) {
-        if (localStorage.getItem('userToken')) {        
-          xhr.setRequestHeader('Authorization', 
+        if (localStorage.getItem('userToken')) {
+          xhr.setRequestHeader('Authorization',
                 'Bearer ' + localStorage.getItem('userToken'));
         }
       }
@@ -50,5 +49,5 @@ $(document).ready(function() {
         // Just call your API here. The header will be sent
     })
 
-    
+
 });
